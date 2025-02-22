2855aa10e30f1fbc59be79e7c9b63861e86c3a90
Henil Tandel <heniltandel0905@gmail.com>
2025-02-22 19:17:59 +0530

    wrapped with the usercontext and taskcontext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { UserProvider } from "./context/UserContext";
 import "./index.css";
+import TaskProvider from "./context/TaskContext";
+ // ✅ Import TaskProvider
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <UserProvider>
+      <TaskProvider> {/* ✅ Wrap App in TaskProvider */}
+        <App />
+      </TaskProvider>
+    </UserProvider>
+  </BrowserRouter>
 );
